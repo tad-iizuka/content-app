379fda7f4526459cb82f0b05db521db2d8db396e
tad-iizuka <tad.iizuka@gmail.com>
2024-07-24 20:16:50 +0900

    delete: vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: [
-    "vuetify/lib/styles/main.sass"
-  ],
+  // css: [
+  //   "vuetify/lib/styles/main.sass"
+  // ],
 
-  build: {
-      transpile: ["vuetify"]
-  },
+  // build: {
+  //     transpile: ["vuetify"]
+  // },
 
-  modules: [
-    '@pinia/nuxt',
-  ],
-
+  // modules: [
+  //   '@pinia/nuxt',
+  // ],
   devtools: { enabled: true },
-
   // modules: ['@nuxt/content', "@nuxt/image"],
   routeRules: {
     '/': { prerender: true }
@@ -22,6 +20,5 @@ export default defineNuxtConfig({
   app: {
     baseURL: '/content-app/',
   },
-
   compatibilityDate: '2024-07-24'
 })
