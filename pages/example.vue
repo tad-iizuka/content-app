a6c0e4fb41cad914c0cf7de68c7dc40c4b4e8e0e
tad-iizuka <tad.iizuka@gmail.com>
2024-07-25 20:14:55 +0900

    add: contents

--- pages/example.vue
+++ pages/example.vue
@@ -1,42 +1,25 @@
-<!-- pages/example.vue -->
 <template>
   <div class="flex justify-center items-center">
-    <div v-if="path" class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <!-- <a href="#"> -->
-            <img class="rounded-t-lg" :src="`${ $config.app.baseURL }` + path" alt="" />
-        <!-- </a> -->
-        <div class="p-5">
-            <a href="#">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-            <!-- <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Read more
-                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
-            </a> -->
-        </div>
+    <div v-if="path" class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+      <h5 class="text-center mt-6 mb-2 text-5xl font-bold tracking-tight text-gray-900">{{ name }}</h5>
+      <img class="py-4 px-4 rounded-t-lg" :src="`${$config.app.baseURL}` + path" alt="" />
+      <div class="p-5">
+        <p class="mb-3 px-4 font-normal text-gray-700 dark:text-gray-400">{{ description }}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-// const { content } = await import('~/assets/content.json')
-  const path = ref('');
-  const route = useRoute()
-  if (route.query.id) {
-    console.log(route.query.id)
-    const users = await import(`~/assets/content.json`)
-    path.value = users.default[route.query.id].image
-  }
-  // console.log(path.value)
-  // asyncData() {
-  //   const users = require(`~/assets/users.json`)
-  //   return {
-  //     users
-  //   }
-  // }
-  // const route = useRoute()
-  // console.log(route.query.id)
+const path = ref('');
+const name = ref('');
+const description = ref('');
+const route = useRoute()
+
+if (route.query.id) {
+  const users = await import(`~/assets/content.json`)
+  path.value = users.default[route.query.id].image
+  name.value = users.default[route.query.id].name
+  description.value = users.default[route.query.id].description
+}
 </script>
