426821552cc96e619606651eaaebe7685360ac0e
tad-iizuka <tad.iizuka@gmail.com>
2024-07-25 18:37:45 +0900

    add: index

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,15 @@
 <template>
-  <div>
-    <h1>Welcome to the homepage</h1>
-    <AppAlert>
-      This is an auto-imported component
-    </AppAlert>
+  <div class="h-screen w-screen flex justify-center items-center">
+    <div class="m-auto">
+      <div class="text-center mb-4 text-5xl leading-none text-gray-900">すぎのこ農園</div>
+      <div class="text-center mb-4 text-base leading-none dark:text-gray-400">Proof of Concept</div>
+    </div>
   </div>
+  <footer class="flex justify-center items-center">
+    <div class="max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span class="text-xs text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://ubunifu.co/"
+          class="hover:underline">UBUNIFU™</a>. All Rights Reserved.
+      </span>
+    </div>
+  </footer>
 </template>
